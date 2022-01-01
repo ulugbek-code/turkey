116600e6d45687888792a0c44aaeab076fd81ce5
Ulugbek Mannopov <umannopov3@gmail.com>
2022-01-01 13:32:27 +0500

    redirecting

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,21 +1,21 @@
 <template>
-  <h1>home page</h1>
-  <div>
+  <h1></h1>
+  <!-- <div>
     <router-link to="/valid.php.Guid=7fcae9d7-d9a0-11eb-9744-0358549"
       >one</router-link
     >
     <router-link to="/valid-php-Guid=1jhi43kw-ad24-23ad=2341-0762968"
       >second</router-link
     >
-  </div>
+  </div>  -->
 </template>
 
-<!-- <script>
+<script>
 export default {
   created() {
     window.location.href = "https://medos.tobb.org.tr/";
   },
 };
-</script> -->
+</script>
 
 <style></style>
